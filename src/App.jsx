3143a95aa48e39ac1d238e9f266b5dc68862b432
Lukasz Bros <lukasz.bros.sp@gmail.com>
2024-03-18 16:04:55 +0100

    background changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Experience } from "./components/Experience";
 function App() {
   return (
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+      <color attach="background" args={["#000000"]} />
       <Experience />
     </Canvas>
   );
