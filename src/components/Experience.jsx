23d87dfa6766f15bad3155c70aaf87f340aedb57
Lukasz Bros <lukasz.bros.sp@gmail.com>
2024-03-19 15:04:22 +0100

    particles added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,23 @@
 import { OrbitControls } from "@react-three/drei";
 
+const count = 1000;
+const positions = new Float32Array(count * 3);
+
+let i;
+for(i = 0; i < count * 3; i++) {
+  positions[i] = Math.random();
+}
+
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <points position={[0, 0, 0]}>
+        <bufferGeometry>
+          <bufferAttribute attach="attributes-position" count={positions.length / 3} array={positions} itemSize={3} />
+        </bufferGeometry>
+        <pointsMaterial size={0.02} sizeAttenuation={true}  />
+      </points>
     </>
   );
 };
