62d720285e8c6073106f390e120c60fc743dc787
Lukasz Bros <lukasz.bros.sp@gmail.com>
2024-03-20 14:04:01 +0100

    camera corrections

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [10, 10, 10], fov: 30 }}>
       <color attach="background" args={["#000000"]} />
       <Experience />
     </Canvas>
