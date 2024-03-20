257d7e3ae6c2e8ec8bdd4157adcc7bce413c4752
Lukasz Bros <lukasz.bros.sp@gmail.com>
2024-03-20 13:58:52 +0100

    Particles in a component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,54 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
-
-const dimentionSize = 5;
-const particlesCount = dimentionSize * dimentionSize * dimentionSize;
-
-const positions = new Float32Array(particlesCount * 3);
-
-let i;
-
-let currentParticlePosition = [0, 0, 0]
-let allPositions = [];
-
-for(i = 0; i < particlesCount; i++) {
-  positions[i * 3] = currentParticlePosition[0];
-  positions[i * 3 + 1] = currentParticlePosition[1];
-  positions[i * 3 + 2] = currentParticlePosition[2];
-
-  generateNextParticle();
-}
-
-function generateNextParticle () {
-  const maxSize = dimentionSize - 1;
-
-  if(currentParticlePosition[1] === maxSize && currentParticlePosition[0] === maxSize) {
-    currentParticlePosition[0] = 0;
-    currentParticlePosition[1] = 0;
-    currentParticlePosition[2]++;
-
-    return;
-  }
-
-  if(currentParticlePosition[0] === maxSize) {
-    currentParticlePosition[0] = 0;
-    currentParticlePosition[1]++;
-
-    return;
-  }
-
-  currentParticlePosition[0]++;
-}
+import Particles from "./Particles";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <points position={[-0.5, -0.5, -0.5]}>
-        <bufferGeometry>
-          <bufferAttribute attach="attributes-position" count={positions.length / 3} array={positions} itemSize={3} />
-        </bufferGeometry>
-        <pointsMaterial size={0.5} sizeAttenuation={true}  />
-      </points>
+      <Particles />
     </>
   );
 };
