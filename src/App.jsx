5d5ef859f1f8806ca24630ab5b0b901c13b51905
Lukasz Bros <lukasz.bros.sp@gmail.com>
2024-03-21 16:33:31 +0100

    selected particle colored

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [20, 20, 20], fov: 40 }}>
+    <Canvas shadows camera={{ position: [5, 6, 10], fov: 30 }}>
       <color attach="background" args={["#000000"]} />
       <Experience />
     </Canvas>
--- src/components/Particles/Particles.jsx
+++ src/components/Particles/Particles.jsx
@@ -1,16 +1,19 @@
 import { useTexture } from '@react-three/drei';
-import getQubePositions from './getQubePositions'
+import getQubePositions from './getQubePositions';
+import getQubeColors from './getQubeColors';
 
 export default function Particles() {
-  const qubeSize = 10;
+  const qubeSize = 4;
   const positions = getQubePositions(qubeSize);
+  const colors = getQubeColors(qubeSize, [1, 1, 2], 2);
 
   const texture = useTexture('src/assets/particle.png');
   
   return <points position={[-qubeSize / 2, -qubeSize / 2, -qubeSize / 2]}>
   <bufferGeometry>
     <bufferAttribute attach="attributes-position" count={positions.length / 3} array={positions} itemSize={3} />
+    <bufferAttribute attach="attributes-color" count={colors.length / 3} array={colors} itemSize={3} />
   </bufferGeometry>
-  <pointsMaterial depthWrite={false} transparent={true} alphaMap={texture} size={2} sizeAttenuation={true}  />
+  <pointsMaterial vertexColors={true} depthWrite={false} transparent={true} alphaMap={texture} size={4} sizeAttenuation={true}  />
 </points>
 }
