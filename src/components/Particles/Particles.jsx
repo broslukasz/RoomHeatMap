b2c761a50999983b3458d1fd33b38029e5646bda
Lukasz Bros <lukasz.bros.sp@gmail.com>
2024-03-21 11:25:40 +0100

    custom particle

--- src/components/Particles/Particles.jsx
+++ src/components/Particles/Particles.jsx
@@ -1,12 +1,16 @@
+import { useTexture } from '@react-three/drei';
 import getQubePositions from './getQubePositions'
 
 export default function Particles() {
   const qubeSize = 10;
   const positions = getQubePositions(qubeSize);
+
+  const texture = useTexture('src/assets/particle.png');
+  
   return <points position={[-qubeSize / 2, -qubeSize / 2, -qubeSize / 2]}>
   <bufferGeometry>
     <bufferAttribute attach="attributes-position" count={positions.length / 3} array={positions} itemSize={3} />
   </bufferGeometry>
-  <pointsMaterial size={0.5} sizeAttenuation={true}  />
+  <pointsMaterial depthWrite={false} transparent={true} alphaMap={texture} size={2} sizeAttenuation={true}  />
 </points>
 }
